f953335f2eed9475366e34a08fa87d8941c3f3c9
Edmond Irani Liu <edmond.irani@tum.de>
2021-02-17 23:19:16 +0100

    update code

--- src/main/java/org/springframework/samples/petclinic/rest/importcsv/ImportCSV.java
+++ src/main/java/org/springframework/samples/petclinic/rest/importcsv/ImportCSV.java
@@ -9,6 +9,7 @@ import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.security.access.prepost.PreAuthorize;
+// not sure if doing the star import is a good practice in Java
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -26,113 +27,87 @@ public class ImportCSV {
     @Autowired
     private ClinicService clinicService;
 
+    public enum PetType {
+        DOG, CAT;
+    }
+
+    private class Entry{
+        String petName;
+        String date;
+        PetType petType;
+        String ownerName;
+        String operation;
+
+        private Entry(){
+            // not sure which keyword does Java use to refer to itself, it is assumed 'this' is the right one
+            this.petName = petName
+            this.date = date
+            this.petType = petType
+            this.ownerName = ownerName
+            this.operation = operation
+        }
+    }
+
+    public List<Entry> parseCSV(String csv){
+        List<Entry> entries = new List<Entry>();
+
+        // split csv with '\n' sign to get its rows
+        List<String> lines = csv.split('\n');
+
+        Boolean isParsable = True;
+        for(line: lines){
+            // it is assumed the count function return the number of occurence of ';' in the text
+            if(line.count(';') != 4){
+                isParsable = False;
+                break;
+            }
+        }
+
+        // early termination due to the fact that ';' are not correct in at least an entry
+        if(!isParsable){
+            return entries;
+        }
+
+        // now parse each field
+        for(line: lines){
+            // it is assumed that these functions are properly defined to check the validity of each field,
+            // and returns the parsed fields . This can also be wrapped in another function 'toEntry(line)''
+            // some of the validity check of the original code is included, e.g., check for none or multiple pet owner
+            petName = toName(line[0]);
+            date = toDate(line[1]);
+            petType = toPetType(line[2]);
+            ownerName = toName(line[3]);
+            operation = toOperation(line[4]);
+            // ...
+            entries.add(toEntry(petName, date, petType, ownerName, operation));
+        }
+        return entries;
+    }
+
     @PreAuthorize("hasRole(@roles.OWNER_ADMIN)")
     @RequestMapping(value = "importPets",
         method = RequestMethod.POST,
         consumes = "text/plain",
         produces = "application/json")
     public ResponseEntity<List<Pet>> importPets(@RequestBody String csv) {
+        // parse CSV and do validity check
 
-        int i = 0;
+        List<Entry> entries = parseCSV(csv);
         List<Pet> pets = new LinkedList<Pet>();
-        Pet pet;
 
-        do {
-            pet = new Pet();
+        for(entry: entries){
+            // we assume pet now accepts an entry, and set its attributes accordingly, e.g. : pet.setName(entry.petName)
+            Pet pet(entry);
 
-            String field = "";
-            while (i < csv.length() && csv.charAt(i) != ';') {
-                field += csv.charAt(i++);
-            }
-            i++;
-
-            pet.setName(field);
-
-            field = "";
-            while (i < csv.length() && csv.charAt(i) != ';') {
-                field += csv.charAt(i++);
-            }
-            i++;
-
-            try {
-                pet.setBirthDate((new SimpleDateFormat("yyyy-MM-dd")).parse(field));
-            } catch (ParseException e) {
-                HttpHeaders headers = new HttpHeaders();
-                headers.add("errors", "date " + field + " not valid");
-                return new ResponseEntity<List<Pet>>(headers, HttpStatus.BAD_REQUEST);
-            }
-
-            field = "";
-            while (i < csv.length() && csv.charAt(i) != ';') {
-                field += csv.charAt(i++);
-            }
-            i++;
-
-            if (pet != null) {
-                ArrayList<PetType> ts = (ArrayList<PetType>) clinicService.findPetTypes();
-                for (int j = 0; j < ts.size(); j++) {
-                    if (ts.get(j).getName().toLowerCase().equals(field)) {
-                        pet.setType(ts.get(j));
-                        break;
-                    }
-                }
-            }
-
-            field = "";
-            while (i < csv.length() && (csv.charAt(i) != ';' && csv.charAt(i) != '\n')) {
-                field += csv.charAt(i++);
-            }
-
-            if (pet != null) {
-                String owner = field;
-                List<Owner> matchingOwners = clinicService.findAllOwners()
-                    .stream()
-                    .filter(o -> o.getLastName().equals(owner))
-                    .collect(Collectors.toList());
-
-                if (matchingOwners.size() == 0) {
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.add("errors", "Owner not found");
-                    return new ResponseEntity<List<Pet>>(headers, HttpStatus.BAD_REQUEST);
-                }
-                if (matchingOwners.size() > 1) {
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.add("errors", "Owner not unique");
-                    return new ResponseEntity<List<Pet>>(headers, HttpStatus.BAD_REQUEST);
-                }
-                pet.setOwner(matchingOwners.iterator().next());
-            }
-
-            if (csv.charAt(i) == ';') {
-                i++;
-
-                field = "";
-                while (i < csv.length() && csv.charAt(i) != '\n') {
-                    field += csv.charAt(i++);
-                }
-
-                if (field.toLowerCase().equals("add")) {
+            switch(entry.operation.toLowerCase()){
+                case "add":
                     clinicService.savePet(pet);
-                } else {
-                    for (Pet q : pet.getOwner().getPets()) {
-                        if (q.getName().equals(pet.getName())) {
-                            if (q.getType().getId().equals(pet.getType().getId())) {
-                                if (pet.getBirthDate().equals(q.getBirthDate())) {
-                                    clinicService.deletePet(q);
-                                }
-                            }
-                        }
-                    }
-                }
-
-            } else {
-                clinicService.savePet(pet);
+                case "delete":
+                // original logic is wrapped in this function
+                    clinicService.deletePet(pet);
             }
-            i++;
-
-            pets.add(pet);
-
-        } while (i < csv.length() && pet != null);
+            pets.add(pet); 
+        }
 
         return new ResponseEntity<List<Pet>>(pets, HttpStatus.OK);
     }
